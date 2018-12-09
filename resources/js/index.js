b0febfed636c735507bec9a313936dc90837ae87
Omeiza17 <smichael15@alustudent.com>
2018-12-09 19:10:20 +0400

    done with the home layout

--- resources/js/index.js
+++ resources/js/index.js
@@ -4,3 +4,8 @@ let navBarToggle = document.getElementById("js-navbar-toggle");
 navBarToggle.addEventListener("click", () => {
   mainNav.classList.toggle("active");
 });
+
+let docs = document.getElementById('docs');
+docs.addEventListener("click", () => {
+  window.open('resources/doc/Integrated Project 2 Specification August 2018 V1.0.pdf', '_blank');
+});
